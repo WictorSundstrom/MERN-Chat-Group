2a5ae56d88b33ec4195bda595c357b677147c81a
Wictor Sundstrom <wictor.sundstrom@hotmail.com>
2019-09-11 00:32:46 +0200

    added a friends js for future use

---
+++ frontend-react/src/Components/Friends.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Friends extends React.Component {
+
+    render() {
+        return (
+            <div>
+                Friends
+            </div>
+        )
+    }
+}
+
+export default Friends
