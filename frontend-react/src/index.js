7131f620120116cdf62b3cdb1eaf32aeb5017d2d
Wictor Sundstrom <wictor.sundstrom@hotmail.com>
2019-09-11 00:33:18 +0200

    added semantic-ui

--- frontend-react/src/index.js
+++ frontend-react/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import 'tachyons';
 import App from './Components/App';
 import './Components/style.css'
+import 'semantic-ui-css/semantic.min.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
