cb52b430b7d62d1e28848e528be11ce71cd541dc
Wictor Sundstrom <wictor.sundstrom@hotmail.com>
2019-09-11 00:32:27 +0200

    made it so / redirects to welcome

--- frontend-react/src/Components/Home.js
+++ frontend-react/src/Components/Home.js
@@ -1,12 +1,11 @@
 import React from 'react'
+import { Redirect } from 'react-router-dom'
 
 class Home extends React.Component {
 
     render() {
         return (
-            <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5' style={{width: 770}}>
-                HOME
-            </div>
+          <Redirect to="/Welcome" />
         )
     }
 }
