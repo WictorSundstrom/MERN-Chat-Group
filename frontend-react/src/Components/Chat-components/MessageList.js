abfffede4b5e13db01f06bd6c82317b9e50f2b0c
Wictor Sundström <wictor.sundstrom@hotmail.com>
2019-09-17 14:42:58 +0200

    added socket io

--- frontend-react/src/Components/Chat-components/MessageList.js
+++ frontend-react/src/Components/Chat-components/MessageList.js
@@ -1,41 +1,76 @@
-import React from 'react'
+import React from "react";
+import io from "socket.io-client";
 
-//dummy datat for the message list
-const DUMMY_DATA = [
-    {
-        senderId: 'Jörgen',
-        text: 'Hey, how is it going?'
-    },
-    {
-        senderId: 'janedoe',
-        text: 'Great! How about you?'
-    },
-    {
-        senderId: 'Jörgen',
-        text: 'Good to hear! I am great as well'
+class Chat extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            username: '',
+            message: '',
+            messages: []
+        };
+        
+
+        this.socket = io('localhost:3001');
+
+        this.socket.on('RECEIVE_MESSAGE', function(data){
+            addMessage(data);
+        });
+
+        const addMessage = data => {
+            console.log(data);
+            this.setState({messages: [...this.state.messages, data]});
+            console.log(this.state.messages);
+        };
+
+        this.sendMessage = ev => {
+            ev.preventDefault();
+            this.socket.emit('SEND_MESSAGE', {
+                author: this.state.username,
+                message: this.state.message
+            })
+            this.setState({message: ''});
+
+        }
     }
-]
 
-class MessageList extends React.Component {
+    componentDidMount() {
+        
+    }
+
     render(){
-        return(
-            <div className="message-list">
-                {/*we take dummy data and map it in a arrow function
-                message: is the data
-                index: is the key */}
-                {DUMMY_DATA.map((message, index) => { 
-                    return(
-                        <div key={index} className="message">
-                            {/* message wich is the dummy data text and senderId the user.
-                                message(what do we want to get).senderId(where do we get it)*/}
-                            <div className="message-username">{message.senderId}</div> 
-                            <div className="message-text">{message.text}</div>
+        return (
+             <div className="container">
+                <div className="row">
+                    <div className="col-4">
+                        <div className="card">
+                            <div className="card-body">
+                                <div className="card-title">Global Chat</div>
+                                <hr/>
+                                <div className="messages">
+                                    {this.state.messages.map(message => {
+                                        return (
+                                            <div>{message.author}: {message.message}</div>
+                                        )
+                                    })}
+                                </div>
+
+                            </div>
+                            <div className="card-footer">
+                                <input type="text" placeholder="Username" value={this.state.username} onChange={ev => this.setState({username: ev.target.value})} className="form-control"/>
+                                <br/>
+                                <input type="text" placeholder="Message" className="form-control" value={this.state.message} onChange={ev => this.setState({message: ev.target.value})}/>
+                                <br/>
+                                <button onClick={this.sendMessage} className="btn btn-primary form-control">Send</button>
+                            </div>
                         </div>
-                    )
-                })}
+                    </div>
+                </div>
             </div>
+
         );
     }
 }
 
-export default MessageList
+export default Chat;
