6c3f7a2c6f6e36a86e1c03a7d516f6cea7b3f1b6
Wictor Sundström <wictor.sundstrom@hotmail.com>
2019-09-17 14:44:04 +0200

    changed chat from functional component to class

--- frontend-react/src/Components/App.js
+++ frontend-react/src/Components/App.js
@@ -4,12 +4,10 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Home } from './Home'
 import { Login } from './Login-components/Login'
 import { Signup } from './Login-components/Signup'
-import { Chat } from './Chat'
+import Chat from './Chat'
 import { Welcome } from './Welcome'
 import { Friends } from './Friends'
 import { ProtectedRoute } from './Routes/ProtectedRoute'
-
-
 class App extends React.Component {
 
     render () {
