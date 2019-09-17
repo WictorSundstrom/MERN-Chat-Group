0ad2aa7665817347536461eed175eff76a6cfc7e
Wictor Sundström <wictor.sundstrom@hotmail.com>
2019-09-17 14:57:15 +0200

    removed css.

--- frontend-react/src/index.js
+++ frontend-react/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App';
 import './Components/style.css'
-import './Components/chatStyle.css'
 import 'semantic-ui-css/semantic.min.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
